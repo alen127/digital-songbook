3c2c3528c66610f21227a6f0b0e7f855069be8e8
alen127 <alenbogadi@proton.me>
2024-06-06 18:28:43 +0200

    Add migrations

---
+++ DAL/Migrations/20240606103840_ChangeStrummingPatternToIntArray.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeStrummingPatternToIntArray : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StrummingPattern",
+                table: "Songs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StrummingPattern",
+                table: "Sections",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StrummingPattern",
+                value: "[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]");
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StrummingPattern",
+                value: "[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StrummingPattern",
+                table: "Songs",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StrummingPattern",
+                table: "Sections",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StrummingPattern",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StrummingPattern",
+                value: null);
+        }
+    }
+}
---
+++ DAL/Migrations/20240606104751_ChangeStrummingPatternToBooleanArray.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeStrummingPatternToBooleanArray : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StrummingPattern",
+                value: "[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]");
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StrummingPattern",
+                value: "[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StrummingPattern",
+                value: "[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]");
+
+            migrationBuilder.UpdateData(
+                table: "Songs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StrummingPattern",
+                value: "[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]");
+        }
+    }
+}
